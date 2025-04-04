d662d1341252852cc22e809f851c0f528da8ca9c
Bingzhi Zhao <bz2746@nyu.edu>
2025-04-03 23:32:35 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,24 +13,36 @@ const markerOptions = {
 };
 
 // create the popup
-const popup1 = new mapboxgl.Popup({ offset: 25 }).setText(
-    '猫狗第一次见面的地方，害羞'
-);
+const popup1 = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+    <div style="text-align:left;">
+      <p>猫狗第一次见面的地方，害羞</p>
+      <img src="https://github.com/tonybzzhao/class-3-map/blob/main/images/kx.JPG" alt="kx" style="width:100%; height:auto;">
+    </div>
+  `);
 
 // create the popup
-const popup2 = new mapboxgl.Popup({ offset: 25 }).setText(
-    '猫狗一起上学的地方，开心鸭'
-);
+const popup1 = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+    <div style="text-align:left;">
+      <p>猫狗一起上学的地方，开心鸭</p>
+      <img src="https://github.com/tonybzzhao/class-3-map/blob/main/images/ucl.JPG" alt="ucl" style="width:100%; height:auto;">
+    </div>
+  `);
 
 // create the popup
-const popup3 = new mapboxgl.Popup({ offset: 25 }).setText(
-    '第一代猫狗窝，地址：Flat 86, Witley Court, Coram Street, London WC1N 1HD'
-);
+const popup1 = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+    <div style="text-align:left;">
+      <p>第一代猫狗窝，地址：Flat 86, Witley Court, Coram Street, London WC1N 1HD</p>
+      <img src="https://github.com/tonybzzhao/class-3-map/blob/main/images/home1.JPG" alt="home1" style="width:100%; height:auto;">
+    </div>
+  `);
 
 // create the popup
-const popup4 = new mapboxgl.Popup({ offset: 25 }).setText(
-    '第二代猫狗窝，猫咪2023年4月来看狗狗。地址：Flat 403, 10 Hosier Lane, London, EC1A 9LS'
-);
+const popup1 = new mapboxgl.Popup({ offset: 25 }).setHTML(`
+    <div style="text-align:left;">
+      <p>第二代猫狗窝，猫咪2023年4月来看狗狗。地址：Flat 403, 10 Hosier Lane, London, EC1A 9LS</p>
+      <img src="https://github.com/tonybzzhao/class-3-map/blob/main/images/home2.JPG" alt="home2" style="width:100%; height:auto;">
+    </div>
+  `);
 
 const kx = new mapboxgl.Marker(markerOptions)
     .setLngLat([-0.12460, 51.53130])
