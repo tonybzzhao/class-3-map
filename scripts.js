84958447909aa32952a68ffe403d83ec53068d4c
Bingzhi Zhao <bz2746@nyu.edu>
2025-04-03 20:18:51 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,13 +8,26 @@ const map = new mapboxgl.Map({
     zoom: 8 // starting zoom
 });
 
+const markerOptions = {
+    color: '#005D63', // default color for the marker
+};
 
- // Create a default Marker and add it to the map.
- const home1 = new mapboxgl.Marker({ color: 'blue', rotation: 0 })
+ // create the popup
+const popup1 = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Flat 86, Witley Court, Coram Street, London WC1N 1HD'
+);
+
+ // create the popup
+ const popup2 = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Flat 403, 10 Hosier Lane, London, EC1A 9LS'
+);
+
+ const home1 = new mapboxgl.Marker(markerOptions)
  .setLngLat([-0.12657, 51.52405])
+ .setPopup(popup1)
  .addTo(map);
 
-// Create a default Marker, colored black, rotated 45 degrees.
-const home2 = new mapboxgl.Marker({ color: 'blue', rotation: 0 })
+const home2 = new mapboxgl.Marker(markerOptions)
  .setLngLat([-0.10267, 51.51769])
+ .setPopup(popup2)
  .addTo(map);
